fff70b4f5118a53dff80916f376cdc894d678c20
quesuerte <carson.blinn@gmail.com>
2025-09-15 19:17:24 -0400

    Adding install instructions, making fzf install OS agnostic

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ This is an extremely baseline setup of `nvim` to provide necessary IDE tooling.
 
 I've tried only to include the most official libraries, and completely avoided using a package manager.
 
+# Installing `neovim`
+## Windows
+Use the installer method, it'll work
+
+## Linux
+`apt` repositories are pretty far behind. Follow the directions in the [Building Neovim](https://github.com/neovim/neovim/wiki/Building-Neovim/688be28f98c18e73b5043879b5963287a9b13d6c) page to build from source.
+
 # Setup
 If cloning the repo, plugins can be downloaded automatically using:
 `git clone --recurse-submodules -b minimal https://github.com/quesuerte/nvim.git`
--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,13 +1,16 @@
 -- git clone https://github.com/nvim-telescope/telescope.nvim.git ./pack/nvim/start/telescope
 -- Download latest release and add to path from following repo:
 -- https://github.com/BurntSushi/ripgrep/releases
+-- Or, for linux, run the following command:
+-- `apt install ripgrep`
 
 local f=io.open("~/.config/nvim/pack/nvim/start/telescope-fzf-native/build/libfzf.dll","r")
 if f~=nil then io.close() else
--- replace gcc command below with the following if using cl.exe
--- cl /O2 /W3 /LD src/fzf.c /Fe:build/libfzf.dll
---os.execute('powershell -Command "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll"')
-os.execute('bash -c "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll"')
+    if package.config:sub(1,1) == "\\" then
+        os.execute('powershell -Command "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir -p build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll"')
+    else
+        os.execute('bash -c "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir -p build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.so"')
+    end
 end
 require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
