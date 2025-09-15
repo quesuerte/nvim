81c112a877a138e177927b12d356f1d1a20abd58
quesuerte <carson.blinn@gmail.com>
2025-09-15 18:42:44 -0400

    Don't actually need to set rust config

--- lua/setup/nvim-lspconfig.lua
+++ lua/setup/nvim-lspconfig.lua
@@ -1,10 +1,10 @@
 -- nvim-lspconfig setup
 -- Make sure you've run git clone https://github.com/neovim/nvim-lspconfig ./pack/nvim/start/nvim-lspconfig
-vim.lsp.config['rust-analyzer'] = {
-    cmd = { 'rust-analyzer' },
-    filetypes = { 'rust' },
-    root_markers = { 'Cargo.toml' }
-}
+--  vim.lsp.config['rust-analyzer'] = {
+--      cmd = { 'rust-analyzer' },
+--      filetypes = { 'rust' },
+--      root_markers = { 'Cargo.toml' }
+--  }
 
 vim.lsp.enable('pyright')
 vim.lsp.enable('rust-analyzer')
