d77428f4fc46d28ff5f6f5eea9fa0122058a620d
Carson Blinn <blinnc@uw.edu>
2025-12-30 10:57:58 -0500

    Fixing MVSC based installs

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -7,12 +7,14 @@
 local f=io.open("~/.config/nvim/pack/nvim/start/telescope-fzf-native/build/libfzf.dll","r")
 if f~=nil then io.close() else
     if package.config:sub(1,1) == "\\" then
-        os.execute('powershell -Command "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir -p build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll"')
+        require('telescope')
+        -- Windows MVSC doesn't come with anything to build the plugin, just use the default fzf
+        -- os.execute('powershell -Command "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir -p build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll"')
     else
         os.execute('bash -c "cd ~/.config/nvim/pack/nvim/start/telescope-fzf-native/; mkdir -p build; gcc -O3 -Wall -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.so"')
+        require('telescope').load_extension('fzf')
     end
 end
-require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
